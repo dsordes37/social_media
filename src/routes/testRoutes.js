c9bb042dbf5ac09c7acb0be6953156e2f030b831
dsordes37 <dsordesdinho342@gmail.com>
2025-05-25 01:12:39 -0300

    Apaga o arquivo testRoutes.

--- src/routes/testRoutes.js
+++
@@ -1,8 +0,0 @@
-const testController = require('../controllers/testController');
-const express = require('express');
-const router = express.Router();
-
-
-router.get('/', testController.getAllUsers)
-
-module.exports = router
